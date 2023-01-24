4bb9b4aec6b754acbf7e1a46363154327295bca7
Andras Popovics <popovics@ndras.hu>
2023-01-24 14:04:23 +0100

    Mini-preview

--- apps/studio/pages/ui/[uuid]/index.tsx
+++ apps/studio/pages/ui/[uuid]/index.tsx
@@ -21,7 +21,6 @@ import { type PersistedConfigurationState } from "services/configuration.reducer
 import { StyledSnackbar } from "components/ui/snackbar";
 import { SnackbarCloseReason } from "@mui/material";
 import BugReportIcon from '@mui/icons-material/BugReport';
-import VisibilityIcon from '@mui/icons-material/Visibility';
 import io from 'Socket.IO-client'
 let socket;
 
@@ -34,6 +33,7 @@ const socketInitializer = async () => {
   });
 }
 
+const previewHost = 'http://localhost:3000';
 
 export default function UiDesigner({pkgs, config}: {pkgs: Package[], config: any}) {
   const [open, setOpen] = useState(false);
@@ -89,9 +89,6 @@ export default function UiDesigner({pkgs, config}: {pkgs: Package[], config: any
               <Typography variant="h4" css={{ lineHeight: '40px'}}>UI Designer: {state.title} ({state.uuid})</Typography>
             </div>
             <div css={{display: 'flex', gap: '12px'}}>
-              <Button variant="contained" color="primary" onClick={() => window.open(`http://localhost:3000/${state.uuid}`, '_blank')}>
-                <VisibilityIcon />
-              </Button>
               <Button variant="contained" color="secondary" onClick={() => window.open(`/api/ui/${state.uuid}/config`, '_blank')}>
                 <BugReportIcon />
               </Button>
@@ -107,12 +104,21 @@ export default function UiDesigner({pkgs, config}: {pkgs: Package[], config: any
           </div>
         </Grid>
         <Grid item sx={{display: 'flex', flex: '1 0 auto', gap: '12px', maxWidth: '100%', minHeight: 'calc(100vh - 160px)', maxHeight: 'calc(100vh - 160px)'}}>
-          <Card sx={{ minWidth: 350, overflow: 'auto' }} >
-            <CardContent>
-              <ExtensionSelector packages={pkgs} onClick={add} />
-              <Navigation select={select} />
-            </CardContent>
-          </Card>
+          <div css={{ minWidth: 350, overflow: 'auto', display: 'flex', gap: 16, flexDirection: 'column' }}>
+            <Card sx={{ minWidth: 350, overflow: 'auto', flexGrow: '1' }} >
+              <CardContent>
+                <ExtensionSelector packages={pkgs} onClick={add} />
+                <Navigation select={select} />
+              </CardContent>
+            </Card>
+            <Card sx={{ minWidth: 350, overflow: 'hidden', flexGrow: '0', marginBottom: '1px', width: '350px', height: '200px', position: 'relative' }} >
+              <iframe src={`${previewHost}/${state.uuid}`} css={{ transformOrigin: '0 0', transform: 'scale(0.25)', width: '1400px', height: '800px', border: 'none' }} />
+              <button
+                css={{opacity: 0, width: '350px', height: '200px', position: 'absolute', top: 0, left: 0, cursor: 'pointer'}}
+                onClick={() => window.open(`${previewHost}/${state.uuid}`, '_blank')}
+              ></button>
+            </Card>
+          </div>
           { state.selected && <ExtensionEditor uuid={state.selected} update={update} /> }
         </Grid>
       </Grid>
